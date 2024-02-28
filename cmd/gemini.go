61c07e39788764eaaef540092b6280d316bbcd47
haokok <hao.nguyentan@unified.com>
2024-02-28 10:21:56 +0700

    initialize

---
+++ cmd/gemini.go
@@ -0,0 +1,88 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+const KEY = "GEMINI_API_KEY"
+
+// geminiCmd represents the gemini command
+var geminiCmd = &cobra.Command{
+	Use:   "gemini",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		chatWithGemini(cmd, args)
+	},
+}
+
+func chatWithGemini(cmd *cobra.Command, args []string) {
+	apiValue := os.Getenv(KEY)
+
+	if apiValue == "" {
+		fmt.Println("API key for gemini does not exit.")
+		os.Exit(1)
+	}
+
+	inputText := strings.Join(args, " ")
+	if inputText == "" {
+		fmt.Println("Input text is empty. Existing...")
+		return
+	} else {
+		ctx := context.Background()
+		client, err := genai.NewClient(ctx, option.WithAPIKey(apiValue))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer client.Close()
+
+		model := client.GenerativeModel("gemini-pro")
+		prompt := genai.Text(inputText)
+		iter := model.GenerateContentStream(ctx, prompt)
+		for {
+			resp, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(resp.Candidates[0].Content.Parts[0])
+		}
+
+	}
+
+}
+
+func init() {
+	rootCmd.AddCommand(geminiCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// geminiCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// geminiCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import (
+	"github.com/tanhaok/hey/cmd"
+)
+
+func main() {
+
+	cmd.Execute()
+
+}
